ce24ca368ed5dd3c8c806ca3ab3a95ad8c82dfdf
Michael Kaye <1917473+michaelkaye@users.noreply.github.com>
2023-05-03 17:08:03 +0100

    Create authentication methods in adapter.

---
+++ src/actions/auth.ts
@@ -0,0 +1,32 @@
+import { Page }  from "playwright";
+
+module.exports = {
+    "register": async ({ page, data }: { page: Page, data: any }) => {
+        await page.getByTestId("home_register").click(); 
+        await page.getByTestId("register_username").fill(data["localpart"]);
+        await page.getByTestId("register_password").fill(data["password"]);
+        await page.getByTestId("register_confirm_password").fill(data["password"]);
+        await page.getByTestId("register_register").click();
+
+        return "registered";
+    },
+    "login": async ({ page, data }: { page: Page, data: any }) => {
+        await page.getByTestId("home_login").click(); 
+        await page.getByTestId("login_username").fill(data["localpart"]);
+        await page.getByTestId("login_password").fill(data["password"]);
+        await page.getByTestId("login_login").click();
+        return "loggedin";
+    },
+    "logout": async ({ page }: { page: Page, data: any }) => {
+        await page.getByTestId("usermenu_open").click(); 
+        await page.getByTestId("usermenu_logout").click();
+        return "logged_out";
+    },
+    "set_display_name": async ({ page, data }: { page: Page, data: any }) => {
+        await page.getByTestId("usermenu_open").click(); 
+        await page.getByTestId("usermenu_user").click();
+        await page.getByTestId("profile_displayname").fill(data["display_name"]);
+        await page.getByTestId("profile_submit").click();
+        return "set_display_name";
+    }
+};
