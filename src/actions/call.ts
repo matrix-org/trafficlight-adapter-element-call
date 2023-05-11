9be21c46f929c375e71474964573a387332b807d
Michael Kaye <1917473+michaelkaye@users.noreply.github.com>
2023-05-11 14:26:54 +0100

    More advanced get_call/lobby_data calls; allow joining a call by URL.

--- src/actions/call.ts
+++ src/actions/call.ts
@@ -5,7 +5,26 @@ import { Page }  from "playwright";
 var call_data_request = 0;
 
 module.exports = {
+    "join_by_url": async ({ page, data} : {page: Page, data: any }) => {
+        const url = data["call_url"];
+        await page.goto(url);
+
+        const needsDisplayName = page.getByTestId("joincall_displayName");
+        const lobbyJoinCall = page.getByTestId("lobby_joinCall");
+
+        await Promise.race([
+            needsDisplayName.waitFor(),
+            lobbyJoinCall.waitFor()
+        ]);
+        const needs_name = await page.getByTestId("joincall_displayName").count() != 0;
+        if (needs_name) {
+            await page.getByTestId("joincall_displayName").fill(data["display_name"]);
+            await page.getByTestId("joincall_joincall").click();
+        }
+        return "joined_by_url";
+    },
     "create_or_join": async ({ page, data }: { page: Page, data: any }) => {
+        await page.waitForTimeout(500); // this seems infrequently unhappy if we fill this in too soon.
         await page.getByTestId("home_callName").fill(data["call_name"]);
         if (data["display_name"]) {
             await page.getByTestId("home_displayName").fill(data["display_name"]);
@@ -37,23 +56,22 @@ module.exports = {
         call_data_request++;
         const muted = await page.getByTestId("videoTile_muted").count() != 0;
         const snapshot_name = `snapshot_video_${call_data_request}_${i}.png`;
-        await page.getByTestId("videoTile_video").screenshot({ path: snapshot_name });
-        const files = {
-            snapshot_name: snapshot_name
-        };
+        await page.getByTestId("preview_video").screenshot({ path: snapshot_name });
+        const files = {};
+	files[snapshot_name] = snapshot_name;
 
         const page_url = page.url();
 	
 	await page.getByTestId("lobby_inviteLink").click();
 
 	// Scream now: We can't access the clipboard directly, but we can make a new page; paste into it; then read what the output was. 
-	const temp_page = context.newPage()
-        await temp_page.setContent(`<div contenteditable></div>`);
-        await temp_page.focus('div');
-        await temp_page.keyboard.press('Control+KeyV');
-        const invite_url = await page.evaluate(() => document.querySelector('div').textContent);
+	const temp_page = await context.newPage();
+        await temp_page.setContent("<div data-testid=paste contenteditable></div>");
+        await temp_page.getByTestId("paste").focus();
+        await temp_page.keyboard.press("Control+KeyV");
+        const invite_url = await temp_page.evaluate(() => document.querySelector("div").textContent);
         await temp_page.close();
-	const call_name = page.getByTestId("roomHeader_roomName").textContent();
+	const call_name = await page.getByTestId("roomHeader_roomName").textContent();
         return { "response": "get_lobby_data", "data": { "muted": muted, "snapshot": snapshot_name, "page_url": page_url, "invite_url": invite_url, "call_name": call_name }, "_upload_files": files };
 
     },
@@ -90,14 +108,14 @@ module.exports = {
 	await page.getByTestId("modal_close").click();
       
 	// Scream now: We can't access the clipboard directly, but we can make a new page; paste into it; then read what the output was. 
-	const temp_page = context.newPage()
-        await temp_page.setContent(`<div contenteditable></div>`);
-        await temp_page.focus('div');
-        await temp_page.keyboard.press('Control+KeyV');
-        const invite_url = await page.evaluate(() => document.querySelector('div').textContent);
+	const temp_page = await context.newPage();
+        await temp_page.setContent("<div data-testid=paste contenteditable></div>");
+        await temp_page.getByTestId("paste").focus();
+        await temp_page.keyboard.press("Control+KeyV");
+        const invite_url = await temp_page.evaluate(() => document.querySelector("div").textContent);
         await temp_page.close();
 
-	const call_name = page.getByTestId("roomHeader_roomName").textContent();
+	const call_name = await page.getByTestId("roomHeader_roomName").textContent();
         return { "response": "get_call_data", "data": { "videos": videos, "page_url": page_url, "invite_url": invite_url, "call_name": call_name}, "_upload_files": files };
 
     },
