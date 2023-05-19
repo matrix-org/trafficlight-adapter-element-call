44f19ef2bf225d4b6c463ce3d5d3faa136f8ffc5
Michael Kaye <1917473+michaelkaye@users.noreply.github.com>
2023-05-19 16:19:00 +0100

    Move video/images/* folder to src/images. Refactor media storage use to not hardcode video or /video

--- src/actions/video.ts
+++ src/actions/video.ts
@@ -1,14 +1,16 @@
+import trafficlightConfig from "../../trafficlight.config.json";
 
 import { copyFile } from "fs/promises";
 module.exports = {
     "set_video_image": async ({data, page}) => {
+        const mediaStorageFolder = process.env["MEDIA_STORAGE_FOLDER"] ?? trafficlightConfig["media-storage-folder"];
         const image = data["image"];
         // Manage two files here, so ffmpeg stats and reopens repeatedly, rather
         // than when it uses a single file which it loads once.
 
         // TODO: investigate alternatives to this as it's a bit janky.
-        await copyFile("video/images/" + image + ".png", "video/images/target0.png");
-        await copyFile("video/images/" + image + ".png", "video/images/target1.png");
+        await copyFile("bin/images/" + image + ".png", mediaStorageFolder+"/target0.png");
+        await copyFile("bin/images/" + image + ".png", mediaStorageFolder+"/target1.png");
 
         // We cycle every second; ensure we've finished cycling before we return
         await page.waitForTimeout(1000);
