d444269d470e3470aae64d4614c577394569f600
Michael Kaye <1917473+michaelkaye@users.noreply.github.com>
2023-08-30 16:45:31 +0100

    Bring trafficlight adapter into line with current element-call

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -18,14 +18,29 @@ module.exports = {
         return "loggedin";
     },
     "logout": async ({ page }: { page: Page, data: any }) => {
-        await page.getByTestId("usermenu_open").click(); 
+        await page.getByTestId("usermenu_open").click();
         await page.getByTestId("usermenu_logout").click();
         return "logged_out";
     },
     "set_display_name": async ({ page, data }: { page: Page, data: any }) => {
-        await page.getByTestId("usermenu_open").click(); 
-        await page.getByTestId("usermenu_user").click();
+        // prefer usermenu over incall_settings.
+        const usermenu_promise = page.getByTestId("usermenu_open").waitFor().then((value) => { return true });
+        const incall_settings_promise = page.getByTestId("incall_settings").waitFor().then((value) => { return false });
+        const usermenu =  await Promise.race([
+            usermenu_promise,
+            incall_settings_promise
+        ]);
+        if (usermenu) {
+        // if out of call:
+            await page.getByTestId("usermenu_open").click(); 
+            await page.getByTestId("usermenu_user").click();
+	} else {
+        // if in call:
+            await page.getByTestId("incall_settings").click();
+        }
+        await page.getByTestId("tab_profile").click();
         await page.getByTestId("profile_displayname").fill(data["display_name"]);
+        await page.getByTestId("modal_close").click(); 
         return "set_display_name";
     }
 };
